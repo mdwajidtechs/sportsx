5264e11c1f1ae01e90176d3197f4903ffb33f134
mdwajidtechs <wajidtech.coms@gmail.com>
2025-08-03 15:18:00 +0530

    you can make

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "8cec6b48a4b1098f35d17ada62fc401d",
+const RESOURCES = {"flutter_bootstrap.js": "e2133c58b19cb7f75c7f9d71ae597924",
 "version.json": "ba6fc4424f2a5471c74b64c98b3aa1ee",
 "index.html": "e6fb3c0c78ec261207206809b87fcc0d",
 "/": "e6fb3c0c78ec261207206809b87fcc0d",
-"main.dart.js": "08f95f9120b9c86094278d0d38ad5d5b",
+"main.dart.js": "57059ab3ce3b9617da6e0cff1fbcedeb",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
